892679f733a3c194344aba00b7c26b4a986193d1
housongting <965825365@qq.com>
2021-03-11 23:41:02 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+import { storeList } from "../api/api.js";
+export default {
+  name: "Home",
+  created() {
+    storeList()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  components: {}
+};
+</script>
